a0432ff3934c3aa383d31cba82d06d4f8f796883
Robert Scully <robert.p.scully@gmail.com>
2025-11-23 23:26:54 +0000

    Update workspace venging configuration

---
+++ terraform/workspaces/main.tf
@@ -0,0 +1,12 @@
+resource tfe_workspace "project_manager_workspace" {
+  name         = "AWS_OIDC_Bootstrap_Workspace"
+  organization = var.terraform_organization
+  project_id   = tfe_project.aws_search_project.id
+  description = "Configures AWS bootstrap configuration details"
+  working_directory = "/terraform/btto"
+  vcs_repo {
+    identifier     = "scullycorp/terrafromcloud-aws-oidc-bootstrapping"
+    branch         = "main"
+    oauth_token_id = var.terraform_oauth_token_id
+  }
+}
--- terraform/workspaces/variables.tf
+++ terraform/workspaces/variables.tf
@@ -1,6 +1,6 @@
 variable terraform_organization {
   type = string
-  description = "The Terraform Cloud organization name to use for the IAM OIDC provider subject claims."
+  description = "(PROJECT VARSET MANAGED) The Terraform Cloud organization name to use for the IAM OIDC provider subject claims."
   validation {
     condition    =  !(strcontains(var.terraform_organization, "*"))
     error_message = "The terraform_organization variable must not contain wildcard characters."
@@ -9,9 +9,14 @@ variable terraform_organization {
 
 variable terraform_project {
   type = string
-  description = "The Terraform Cloud organization name to use for the IAM OIDC provider subject claims."
+  description = "(PROJECT VARSET MANAGED) The Terraform Cloud organization name to use for the IAM OIDC provider subject claims."
   validation {
     condition    =  length(var.terraform_project) >= 3 && length(var.terraform_project) <= 40 && !(strcontains(var.terraform_organization, "*"))
     error_message = "The terraform_project value must be between 3 and 40 characters in length."
   }
+}
+
+variable "terraform_oauth_token_id" {
+  type        = string
+  description = "(PROJECT VARSET MANAGED) The Terraform Cloud OAuth token ID for connecting workspaces to GitHub."
 }
